118de2a072c7a5b35631a907fe4a717609673103
jeancarlo3213 <149128952+jeancarlo3213@users.noreply.github.com>
2024-04-21 18:19:29 -0600

    BASESADD

---
+++ Pages/Clases_Utiles/Solicitud.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyect_two.Pages.Clases_Utiles
+{
+    public class Solicitud
+    {
+        public int Id { get; set; }
+        public int IdOpcion { get; set; }
+        public int IdCliente { get; set; }
+        public string Estado { get; set; }
+        public int? IdTecnico { get; set; }
+        public string Calificacion { get; set; }
+    }
+}
